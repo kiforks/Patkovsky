b4c0f754354f6a515e7865df3411c3d633484e6d
kiforks <ks.kifor@gmail.com>
2020-09-28 20:52:55 +0300

    Fixed header

--- src/js/scripts/fixed_header.js
+++ src/js/scripts/fixed_header.js
@@ -1,13 +1,17 @@
 window.addEventListener('scroll', () => {
   const header = document.querySelector('.header');
   const headerBox = document.querySelector('.header__box');
-  const headerPositionBottom = window.pageYOffset + header.getBoundingClientRect().bottom;
+  const headerLogo = document.querySelector('.header__image');
+  const setLogo = logo => headerLogo.setAttribute('src', `img/svg/header/${logo}.svg`);
+  const headerPositionBottom = window.pageYOffset + header.getBoundingClientRect().bottom - 90;
   const windowPositionTop = window.pageYOffset;
 
   if(headerPositionBottom < windowPositionTop) {
     headerBox.classList.add('header__box--fixed');
+    setLogo('logo_gold');
   } else {
     headerBox.classList.remove('header__box--fixed');
+    setLogo('logo');
   }
 })
 
