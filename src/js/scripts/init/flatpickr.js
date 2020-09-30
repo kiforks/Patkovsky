aa14b0a8f33ae88b2e589599d5b79be365a0d794
kiforks <ks.kifor@gmail.com>
2020-09-30 12:55:21 +0300

    Finished booking section

--- src/js/scripts/init/flatpickr.js
+++ src/js/scripts/init/flatpickr.js
@@ -2,12 +2,22 @@ const checkIn = document.querySelector(".booking__input--check-in");
 const checkOut = document.querySelector(".booking__input--check-out");
 
 function flatpickInit() {
+  const wrapperCheckIn = document.querySelector('.booking__input-wrapper--check-in');
+  const wrapperCheckOut = document.querySelector('.booking__input-wrapper--check-out');
+
   const dateToCheckIn = flatpickr(checkIn, {
     dateFormat: "d-m-Y",
     locale: 'uk',
     // altInput: true,
     // altFormat: "F j, Y",
     minDate: "today",
+    disableMobile: "true",
+    onOpen() {
+      wrapperCheckIn.classList.add('open');
+    },
+    onClose() {
+       wrapperCheckIn.classList.remove('open');
+    }
   });
 
   const dateToCheckOut = flatpickr(checkOut, {
@@ -16,6 +26,13 @@ function flatpickInit() {
     // altInput: true,
     // altFormat: "F j, Y",
     minDate: "today",
+    disableMobile: "true",
+    onOpen() {
+      wrapperCheckOut.classList.add('open');
+    },
+    onClose() {
+       wrapperCheckOut.classList.remove('open');
+    }
   });
 }
 
